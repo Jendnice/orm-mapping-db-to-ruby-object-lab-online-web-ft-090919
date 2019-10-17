5c72362bcb8c3f3f97631ab8220d99c3b4eaba07
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-17 19:39:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,7 +23,8 @@ class Student
 
   def self.find_by_name(name)
     sql = <<-SQL
-      SELECT * FROM students
+      SELECT * 
+      FROM students
       WHERE name = ?
       LIMIT 1 
     SQL
@@ -33,6 +34,17 @@ class Student
     end 
     # find the student in the database given a name
     # return a new instance of the Student class
+    
+     
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+ 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
   end
   
   def save
