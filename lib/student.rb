e8edbcdf365728fb8308febbb9555236ff5b55ab
Jennifer Nicely <pipeline38@hotmail.com>
2019-10-17 19:54:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,6 +42,18 @@ class Student
       self.new_from_db(row)
     end 
   end 
+  
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL 
+     SELECT * 
+     FROM students
+     WHERE grade = 10 
+    SQL
+    
+    DB[:comm].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first(X)
+  end 
 
   def self.find_by_name(name)
     sql = <<-SQL
